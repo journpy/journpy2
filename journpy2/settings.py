f071ff5dd08b0c928ca26b6b29286b3330447632
Uchenna Adubasim <uchadubs@yahoo.com>
2024-06-14 14:47:22 +0100

    updated settings.py

--- journpy2/settings.py
+++ journpy2/settings.py
@@ -20,8 +20,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "journpy.com"]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 INSTALLED_APPS = [
