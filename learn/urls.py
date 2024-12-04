e45ef9035d5ce7e89349953534c167b22639eb61
Uchenna <uchadubs@yahoo.com>
2024-12-04 13:55:00 +0000

    working well

--- learn/urls.py
+++ learn/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LearnPythonDetailView
+from .views import LearnPythonDetailView, LearnDjangoDetailView
 
 urlpatterns = [
     path('python_tutorial/', LearnPythonDetailView.as_view(), name='python_tutorial'),
+    path('django_tutorial/', LearnDjangoDetailView.as_view(), name='django_tutorial'),
     ]
--- learn/views.py
+++ learn/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from .models import LearnPython
+from .models import LearnPython, LearnDjango
 
 
 class LearnPythonDetailView(ListView):
@@ -7,4 +7,11 @@ class LearnPythonDetailView(ListView):
     paginate_by = 1
     model = LearnPython
     template_name = 'learn/python_tutorial.html'
+    context_object_name = 'tutorial_list'
+
+
+class LearnDjangoDetailView(ListView):
+    """ List all Django tutorials available."""
+    model = LearnDjango
+    template_name = 'learn/django_tutorial.html'
     context_object_name = 'tutorial_list'
