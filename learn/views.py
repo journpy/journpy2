21b4f2270cebc61312d1e4d1fc73e49a72ca8e42
Uchenna <uchadubs@yahoo.com>
2024-12-13 09:37:26 +0000

    improved pagination

--- learn/views.py
+++ learn/views.py
@@ -4,8 +4,8 @@ from .models import LearnPython, LearnDjango
 
 class LearnPythonDetailView(ListView):
     """ List all python tutorials available."""
-    paginate_by = 1
     model = LearnPython
+    paginate_by = 1
     template_name = 'learn/python_tutorial.html'
     context_object_name = 'tutorial_list'
 
---
+++ staticfiles/js/base.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById("toggleButton");
+
+
+toggleButton.addEventListener("click", function() {
+    this.classList.add("active");
+});
